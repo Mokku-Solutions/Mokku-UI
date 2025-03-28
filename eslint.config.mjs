7a057df156c5bef5f939517f1e0ee07af1d6ff92
alelex10 <elvizvida@gmail.com>
2025-03-28 18:02:49 -0300

    [1] Install eslint-config-prettier to avoid conflicts with ESlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import someConfig from "some-other-config-you-use";
+// Note the `/flat` suffix here, the difference from default entry is that
+// `/flat` added `name` property to the exported object to improve
+// [config-inspector](https://eslint.org/blog/2024/04/eslint-config-inspector/) experience.
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,4 +18,4 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-export default eslintConfig;
+export default [someConfig, eslintConfigPrettier];
